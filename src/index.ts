ea84c5bef1d7f44fd6d49d98048ee37224177595
kaji <0xkaji@gmail.com>
2021-12-22 09:48:50 +0100

    Update ports

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,7 @@ const start = async () => {
 
   console.log('Init express')
   const app = express()
-  const port = process.env.PORT || 5000
+  const port = process.env.PORT || 2000
 
   setupEvents()
   batchUpdates()
